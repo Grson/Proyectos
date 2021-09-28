5e4c2d8e5e0e56c76e15dfd3c9ed299306316316
Grson <91508918+Grson@users.noreply.github.com>
2021-09-27 23:58:08 -0600

    Add files via upload

---
+++ Multiplo10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiplo10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        System.out.print("Número entero: ");
+        N = sc.nextInt();
+        if(N%10==0)
+           System.out.println("Es múltiplo de 10");   
+        else
+           System.out.println("No es múltiplo de 10");
+    }
+}
